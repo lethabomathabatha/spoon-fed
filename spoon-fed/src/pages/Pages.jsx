d3fb1c954434124e8db18a3566aa504d8ada9c56
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-09 13:33:43 +0200

    added Pages jsx

---
+++ spoon-fed/src/pages/Pages.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import Home from '../components/Home.jsx';
+
+
+export default function Pages() {
+    return (
+        <div>
+            <Home />
+        </div>
+
+        )
+}
