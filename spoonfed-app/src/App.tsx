c986339d0b9d5ee524c2ccd742f3ad1d9cd6fe57
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-08-12 16:57:12 +0200

    sync files and dependencies

--- spoonfed-app/src/App.tsx
+++ spoonfed-app/src/App.tsx
@@ -1,15 +1,10 @@
 import Search from './components/Search'
-import { useState } from 'react'
 import Nav from './components/Nav'
-
-
 import './App.css'
 
 
 
 export default function App() {
-
-
 
   return (
     <>
